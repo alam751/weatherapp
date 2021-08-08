302f88f5b3e47c344869d2024284866c6eea1a58
Zeeshan <alamzeeshan751@gmail.com>
2021-08-08 12:49:34 +0530

    fixed error

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
 # A simple Weather-App using [React.js](https://reactjs.org)
 
 > **This tiny application is like to check the temperature of a city.**
-**This is a very basic app but useful to understand the React Components, states, Effects, Fetching API etc. .**
+> **This is a very basic app but useful to understand the React Components, states, Effects, Fetching API etc. .**
 
 ## Demo
+
 ![weather-app-_online-video-cutter com_-_3_](https://user-images.githubusercontent.com/84847269/128592025-238e0592-a5d9-4872-aca1-0c4c93045da0.gif)
 
 ## Live URL :-
->[weather-app](http://localhost:3000/)
->
+
+> [weather-app](https://weatherapp-gray.vercel.app/)
 
 **OR your can do the following to run in your local machine**
 
-##  Scripts
+## Scripts
 
 Install node modules
 
@@ -20,9 +21,7 @@ Install node modules
 
 In the project directory, you can run:
 
-
 ### `npm start`
 
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
--- src/Tempapp.js
+++ src/Tempapp.js
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const Tempapp = () => {
-  const [city, setCity] = useState(null);
-  const [search, setSearch] = useState("");
+  const [city, setCity] = useState("Mumbai");
+  const [search, setSearch] = useState("Mumbai");
+  const [cityName, setCityName] = useState("");
 
   useEffect(() => {
     const fetchApi = async () => {
@@ -11,35 +12,60 @@ const Tempapp = () => {
       const response = await fetch(url);
       const resJson = await response.json();
       setCity(resJson.main);
+      setCityName("");
     };
     fetchApi();
   }, [search]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearch(cityName);
+  };
+
   return (
     <>
       <div className="box">
-        <div className="inputData">
-          <input
-            type="search"
-            value={search}
-            onChange={(event) => {
-              setSearch(event.target.value);
-            }}
-          />
-        </div>
-        {!city ? (
-          <p>No data found</p>
-        ) : (
-          <div className="info">
-            <h2 className="location">
-              <i className="fas fa-street-view"></i>
-              {search}
-            </h2>
-            <h1 className="temp">{city.temp} °Cel</h1>
-            <h3 className="tempMinMax">
-              Min: {city.temp_min} °Cel || Max: {city.temp_max} °Cel
-            </h3>
+        <div className="container">
+          <div className="heading">
+            <h1>Weather App</h1>
+            <p>Check the weather forecast</p>
           </div>
-        )}
+          <div className="inputData">
+            <input
+              type="text"
+              value={cityName}
+              placeholder="Enter the city name ..."
+              onChange={(event) => {
+                setCityName(event.target.value);
+              }}
+              onKeyUp={(event) => {
+                if (event.key === "Enter") {
+                  setSearch(event.target.value);
+                }
+              }}
+            />
+            <button type="submit" className="btn" onClick={handleSubmit}>
+              search
+            </button>
+          </div>
+        </div>
+        <div className="information">
+          {!city ? (
+            <p className="notFound">
+              Sorry, we did't find any city of name {search}
+            </p>
+          ) : (
+            <div className="info">
+              <h1 className="location">
+                <i className="fas fa-street-view"></i>
+                {search}
+              </h1>
+              <h2 className="temp">{city.temp} °C</h2>
+              <h3 className="tempMinMax">
+                Min: {city.temp_min} °C || Max: {city.temp_max} °C
+              </h3>
+            </div>
+          )}
+        </div>
       </div>
     </>
   );
